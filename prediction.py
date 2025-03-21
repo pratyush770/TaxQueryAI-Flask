87cb05e02d9987d85a0f8693c21821d8ea1cd5f4
pratyush majumdar <pratyushmajumdar27@gmail.com>
2025-03-21 15:07:20 +0530

    flask deployment

--- prediction.py
+++ prediction.py
@@ -1,8 +1,20 @@
 import numpy as np
-from sklearn.linear_model import LinearRegression
 
+class SimpleLinearRegression:  # LinearRegression
+    def __init__(self):
+        self.coef_ = None
+        self.intercept_ = None
 
-def train_prediction_model(df, property_type):  # train and predict
+    def fit(self, X, y):
+        X = np.hstack([np.ones((X.shape[0], 1)), X])  # add bias term for intercept
+        beta = np.linalg.lstsq(X, y, rcond=None)[0]  # solve for coefficients
+        self.intercept_ = beta[0]
+        self.coef_ = beta[1:]
+
+    def predict(self, X):
+        return self.intercept_ + np.dot(X, self.coef_)
+
+def train_prediction_model(df, property_type):  
     # summing columns for tax collection and demand
     tax_sums = {
         "collection": df.select([
@@ -11,7 +23,7 @@ def train_prediction_model(df, property_type):  # train and predict
             f'Tax_Collection_Cr_2015_16_{property_type}',
             f'Tax_Collection_Cr_2016_17_{property_type}',
             f'Tax_Collection_Cr_2017_18_{property_type}'
-        ]).sum().to_numpy()[0],  # convert to numpy array and extract values
+        ]).sum().to_numpy()[0],  
 
         "demand": df.select([
             f'Tax_Demand_Cr_2013_14_{property_type}',
@@ -19,7 +31,7 @@ def train_prediction_model(df, property_type):  # train and predict
             f'Tax_Demand_Cr_2015_16_{property_type}',
             f'Tax_Demand_Cr_2016_17_{property_type}',
             f'Tax_Demand_Cr_2017_18_{property_type}'
-        ]).sum().to_numpy()[0]  # convert to numpy array and extract values
+        ]).sum().to_numpy()[0]
     }
 
     X = np.array([2014, 2015, 2016, 2017, 2018]).reshape(-1, 1)
@@ -27,15 +39,15 @@ def train_prediction_model(df, property_type):  # train and predict
 
     for key in ["collection", "demand"]:
         y = tax_sums[key]
-        model = LinearRegression()  
-        model.fit(X, y)  # fits the model
+        model = SimpleLinearRegression()  
+        model.fit(X, y)
         models[key] = model
 
-    def predict_tax(year):  # function to predict tax collection and demand
-        if year is not None and year >= 2019:  # since we already have data from 2013-18
+    def predict_tax(year):  
+        if year is not None and year >= 2019:  
             return {
-                "predicted_collection": round(models["collection"].predict([[year]])[0], 2),  # for tax collection prediction
-                "predicted_demand": round(models["demand"].predict([[year]])[0], 2)  # for tax demand prediction
+                "predicted_collection": round(models["collection"].predict([[year]])[0], 2),
+                "predicted_demand": round(models["demand"].predict([[year]])[0], 2)
             }
         return None
 
